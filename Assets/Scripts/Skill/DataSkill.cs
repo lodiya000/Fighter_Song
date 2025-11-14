9c45b89cfd462441024d23b825ebaf84754cb807
KID <50666207+KID421@users.noreply.github.com>
2025-11-14 13:23:01 +0800

    feat: 添加技能一般

---
+++ Assets/Scripts/Skill/DataSkill.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Lodiya
+{
+    [CreateAssetMenu(fileName = "DataSkill", menuName = "Lodiya/Skill Normal", order = 0)]
+    public class DataSkill : ScriptableObject
+    {
+        public string skillName;
+        public GameObject skillPrefab;
+        public int skillposture;
+    }
+}
--- Assets/Scripts/Skill/SkillCombo.cs
+++ Assets/Scripts/Skill/SkillCombo.cs
@@ -2,14 +2,11 @@
 
 namespace Lodiya
 {
-    [SerializeField,CreateAssetMenu(menuName = "Lodiya/Skill Combo", order = 0)]
-    public class SkillCombo : ScriptableObject
+    [SerializeField,CreateAssetMenu(menuName = "Lodiya/Skill Combo", order = 1)]
+    public class SkillCombo : DataSkill
     {
         public SkillType[] skillTypes;
-        public string skillName;
-        public GameObject skillPrefab;
-        public int skillposture;
-
+        
         /// <summary>
         /// 技能組合
         /// </summary>
