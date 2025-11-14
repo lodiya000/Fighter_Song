055442392b339c93a128af32bbe336a439135cee
KID <50666207+KID421@users.noreply.github.com>
2025-11-14 14:07:53 +0800

    feat: 符文對調完成

---
+++ Assets/Scripts/Skill/SkillSlot.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace Lodiya
+{
+    /// <summary>
+    /// 技能插槽
+    /// </summary>
+    public class SkillSlot : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+    {
+        private Vector3 originalPoint;
+        private Transform traCanvas;
+        private Transform parent;
+
+        public Image imgSkill { get; private set; }
+        public Sprite sprSkill => imgSkill.sprite;
+        public Color colorSkill => imgSkill.color;
+
+        private void Awake()
+        {
+            originalPoint = transform.localPosition;
+            imgSkill = GetComponent<Image>();
+            traCanvas = GameObject.Find("畫布").transform;
+            parent = transform.parent;
+        }
+
+        public void OnBeginDrag(PointerEventData eventData)
+        {
+            imgSkill.raycastTarget = false;
+            transform.SetParent(traCanvas);
+            transform.SetAsLastSibling();
+        }
+
+        public void OnDrag(PointerEventData eventData)
+        {
+            transform.position = eventData.position;
+        }
+
+        public void OnEndDrag(PointerEventData eventData)
+        {
+            // 如果放開後下方是另一個技能插槽就互換
+            var target = eventData.pointerCurrentRaycast.gameObject;
+            if (target != null && target.TryGetComponent(out SkillSlot targetSkillSlot))
+            {
+                var sprite = imgSkill.sprite;
+                var color = imgSkill.color;
+                imgSkill.sprite = targetSkillSlot.sprSkill;
+                targetSkillSlot.imgSkill.sprite = sprite;
+                imgSkill.color = targetSkillSlot.colorSkill;
+                targetSkillSlot.imgSkill.color = color;
+            }
+
+            transform.SetParent(parent);
+            transform.SetAsFirstSibling();
+            imgSkill.raycastTarget = true;
+            transform.localPosition = originalPoint;
+        }
+    }
+}
